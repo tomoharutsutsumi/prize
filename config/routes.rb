3c2581e3d9f27f255a5684463cbbbdeef437c44f
tomoharu tsutsumi <tomoharutsutsumi@tomoharu-no-MacBook-Air.local>
2018-09-17 15:09:43 +0900

    add 管理画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
   root to: 'awards#index'
   post 'awards/confirm', to: 'awards#confirm'
